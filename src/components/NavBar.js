8df780e9e88c8231fe976658defb2d5d03376692
Joey <30815220+joeylking@users.noreply.github.com>
2021-07-13 18:14:48 -0500

    Basic Functionality

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <div>
+      <ul>
+        <Link to="/">Take Me Home</Link>
+      </ul>
+    </div>
+  );
+}
